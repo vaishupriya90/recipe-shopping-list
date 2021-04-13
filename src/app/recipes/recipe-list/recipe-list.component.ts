aab734a095b79d31750ab62b8234b6f98024b4a4
vaishu <vaishupriya90@gmail.com>
2021-04-13 16:09:07 -0500

    created a template for the recipe list, created an array of dummy recipes in the ts file to loop over and display it in the lsit template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  public recipes = [];
+  public recipes: Recipe[] = [new Recipe("Idli","Lentil Buns","https://4.bp.blogspot.com/-7C3B7VPDVKc/XBKGp3-XlcI/AAAAAAAA4VY/wKe7qrJ-ntQlNN5e1fGksH62duvg6f8-gCLcBGAs/s1600/Instant-Suji-idli.jpg"),new Recipe("Idli","Lentil Buns","https://4.bp.blogspot.com/-7C3B7VPDVKc/XBKGp3-XlcI/AAAAAAAA4VY/wKe7qrJ-ntQlNN5e1fGksH62duvg6f8-gCLcBGAs/s1600/Instant-Suji-idli.jpg")];
   
   constructor() { }
 
